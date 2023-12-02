44b0e2c81467f36c825fff796fdeeec12d7b176d
Roy Prouty <proutyr1@umbc.edu>
2023-12-02 13:07:39 -0500

    Got the class structure to roughly work. Needs some cleaning up and uniform logging with info/debug levels. need to also introduce smoothing need to introduce sigma clipping need to do some outlier detection on calibration frames, too need to make bad pixel masks

--- Observation.py
+++ Observation.py
@@ -1,18 +1,18 @@
 import logging
 from datetime import datetime
 from glob import glob
+from tqdm import tqdm
 
 from astropy.io import fits
+
+import numpy as np
 
 class Redux:
 
     def __init__(self):
-        self.lightLists = []
-        self.darkLists = []
-        self.flatLists = []
-        self.biasLists = []
-
+        self.frameListDict = {}
         self.level = 'INFO'
+        self.FrameLists = []
 
     def makeLog(self):
         LVL = logging.DEBUG if self.level == 'DEBUG' else logging.INFO
@@ -26,87 +26,145 @@ class Redux:
     def findFITS(self):
         self.logger.debug("Got to findFITS function in Redux Class")
         
-        fitsFileList = glob(f"{self.indir}/**/*.fits", recursive=True)
-        self.logger.info(f"Found {len(fitsFileList)} FITS files in {self.indir}.")
+        #Assume all raw light and all calibration frames are in some directory (indir)
+        fitsFileList = glob(f"{self.datadir}/**/*.fits", recursive=True) + glob(f"{self.caldir}/**/*.fits", recursive=True)
+        self.logger.info(f"Found {len(fitsFileList)} FITS files.")
 
-        for fitsFile in fitsFileList:
-            with fits.open(fitsFile)[0] as hdu:
+        #Go through each fits file and create a Frame object for each \
+        #   and construct FrameList objects
+        for fitsFile in tqdm(fitsFileList):
+            with fits.open(fitsFile) as hdul:
+                hdu = hdul[0]
+                #Carve-out for bias and dark frames for which header does not report filter
                 try:
-                    frame = Frame(hdu.data, hdu.header['EXPTIME'], \
-                              hdu.header['FILTER'].upper(), hdu.header['GAIN'], hdu.header['FRAMETYP'].lower())
-                except Exception as e:
-                    print(e)
-                    #We expect bias frames to fail to resolve the 'FILTER' key in the header
-                    frame = Frame(hdu.data, hdu.header['EXPTIME'], \
-                              None, hdu.header['GAIN'], hdu.header['FRAMETYP'].lower())
-                
-                if frame.type == 'light':
-                    pass
+                    #Check to see if this filter was specified to be skipped
+                    if hdu.header['FILTER'].upper() in self.excludeFilter[0]:
+                        continue
+                    if hdu.header['FRAMETYP'].lower() == "flat" and self.no_flat:
+                        continue
+
+                    frame = Frame(hdu.data, hdu.header['FRAMETYP'].lower(), hdu.header['FILTER'].upper(), \
+                                hdu.header['GAIN'], hdu.header['EXPTIME'], hdu.header
+                                )
+                    
+                    #print(f"Added frame {str(frame)}")
+                except Exception as e: #We expect bias and dark frames to fail to resolve the 'FILTER' key in the header
+                    #print(e)
+                    frame = Frame(hdu.data, hdu.header['FRAMETYP'].lower(), None, \
+                                   hdu.header['GAIN'], hdu.header['EXPTIME'], hdu.header
+                                 )
+                    #print(f"Added frame {str(frame)}")
+                    
+                #Create all of the dictionaries!
+                try:
+                    #If frameList for filter is alr defined ...
+                    self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime].append(frame)
+                    self.FrameLists.append(self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime])
+                    #print(f"Added frame {str(frame)} to dictionary!")
+                except KeyError as e: #Couldn't find FrameList for that intTime, trying to add new FrameList for intTime
+                    try:
+                        self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime] = FrameList(frame)
+                        self.FrameLists.append(self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime])
+                    except KeyError as e: #Couldn't find gain, trying to add gain to filter dict
+                        try:
+                            self.frameListDict[frame.type][frame.filter] = {}
+                            self.frameListDict[frame.type][frame.filter][frame.gain] = {}
+                            self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime] = FrameList(frame)
+                            self.FrameLists.append(self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime])
+                        except KeyError as e: #Couldn't find intTime, trying to add intTime to type dict
+                            try: 
+                                self.frameListDict[frame.type] = {}
+                                self.frameListDict[frame.type][frame.filter] = {}
+                                self.frameListDict[frame.type][frame.filter][frame.gain] = {}
+                                self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime] = FrameList(frame)
+                                self.FrameLists.append(self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime])
+                            except KeyError as e: #Couldn't find type dict, trying to add type dict
+                                print(e)         
+    
+    def __str__(self):
+        return f"{self.FrameLists}"
 
 class FrameList(list):
-        def __init__(self):
-            self.first= True
-            self._darksObj = None
-            self._intTime = None
-            self._filter = None
-            self._gain = None
-            self._type = None
+    def __init__(self, frame):
+        self.append(frame)
 
-            self._std = None
-            self._mean = None
-            self._max = None
+        self._darkFrameObj = 0
+        self._flatFrameObj = 1
+        self._master = None
 
-        def append(self, frame):
-            if not self.first:
-                timeCond = self._intTime == frame.intTime
-                gainCond = self._gain == frame.gain
-                filterCond = self._filter == frame.filter 
+    def setDarkFrameObj(self, darkFrameObj):
+        self._darkFrameList = darkFrameObj
 
-                if timeCond and gainCond and filterCond:
-                    self.super(Lights, self).append(frame)
-                else:
-                    raise Exception("This frame has different parameters than others in this list.\n"+\
-                                    f"  Integration Time Match: {timeCond}\n"+\
-                                    f"  Gain Match: {gainCond}\n"+\
-                                    f"  Filter Match: {filterCond}\n")
-            else:
-                self.first = False
-                self._intTime = frame.intTime
-                self._gain = frame.gain
-                self.filter = frame.filter
-                self.super(Lights, self).append(frame)
+    def setFlatFrameObj(self, flatFrameObj):
+        self._flatFrameObj = flatFrameObj
 
-        def __repr__(self):
-            return (self._type, len(self))
+    def __call__(self): #Calculate master frame
+        if self._darkFrameObj == 0:
+            data = np.median([s() for s in self], axis=0)
+        else:
+            data = np.median(self - self._darkFrameObj(), axis=0)
+
+        if self._flatFrameObj != 1:
+            data /= (self._flatFrameObj() / self._flatFrameObj.max)
         
-class Lights(FrameList):
+        self._master = Frame(data, 'master', self[0].filter, self[0].gain, self[0].intTime, self[0].header)
 
-    def __init__(self, frame=None):
-        self._reducedFrame = None
-
-    def setDark(self, darksObj):
-        self.darksObj = darksObj
-
-    def getFilter(self):
-        return self._filter
-
-    def __repr__(self):
-        return ("light", len(self))
-
-
-class Biases(FrameList):
-
-class Darks(FrameList):
+        return self._master
     
-class Flats(FrameList):
+    def append(self, frame):
+        #If this is the first frame to be added to the list
+        if len(self) == 0:
+            #add the frame to the obj(list)
+            super(FrameList, self).append(frame)
+        else: #if this is not the first frame in the list
+            #build a set of conditions to check            
+            typeCond = self[0].type == frame.type
+            filterCond = self[0].filter == frame.filter 
+            gainCond = self[0].gain == frame.gain
+            intTimeCond = self[0].intTime == frame.intTime
 
+            #check if conditions are all met
+            if typeCond and filterCond and gainCond and intTimeCond:
+                #if frame belongs here, add it
+                super(FrameList, self).append(frame)  
+            else: #If frame doesn't belong in this list, say why
+                raise Exception("This frame has different parameters than others in this list.\n"+\
+                                 f"  Frame Type Match: {typeCond}"+\
+                                 f"  Filter Match: {filterCond}\n"+\
+                                 f"  Gain Match: {gainCond}\n"+\
+                                 f"  Integration Time Match: {intTimeCond}"\
+                                )
+    def __repr__(self):
+        return f"{len(self)}x(Type:{self[0].type}, Filter:{self[0].filter}, Gain:{self[0].gain}, IntTime:{self[0].intTime})"
+    
+    def getFrameInfo(self):
+        s = ""
+        for frame in self:
+            s += frame.getInfo()
 
-class Frame(list):
-    def __init__(self, data, intTime, filter, gain, type):
-        self.append(data)
-        self.intTime = intTime
+class Frame:
+    def __init__(self, data, type, filter, gain, intTime, header):
+        self.data = data
+        self.type = type
         self.filter = filter
         self.gain = gain
-        self.type = type
+        self.intTime = intTime
+        self.header = header
 
+        self.std = np.std(data)
+        self.mean = np.mean(data)
+        self.median = np.median(data)
+        self.max = np.max(data)
+        self.min = np.min(data)
+
+    def __call__(self):
+        return self.data
+    
+    def getInfo(self):
+        return f"{self.header['INSTRUME']} {self.header['CCD-TEMP']} {self.header['DATE-OBS']}\n"+\
+            f"Min,Max:{self.min},{self.max} Mean,Median:{self.mean},{self.median} STD: {self.std}\n"+\
+            str(self)
+    
+    def __str__(self):
+        return f"Type:{self.type}, Filter:{self.filter}, Gain:{self.gain}, IntTime:{self.intTime}"
       
--- data_redux.py
+++ data_redux.py
@@ -1,7 +1,10 @@
 import argparse
+from sys import exit
+
+from matplotlib import pyplot as plt
 
 #Authored packages
-from Observation import Redux, Observation
+from Observation import Redux
 
 description = \
 """
@@ -22,16 +25,35 @@ parser.add_argument("--level", metavar="[]", action="store", type=str, required=
                     help="Logging Level. Info or Debug. Debug is more verbose.",\
                     choices=['INFO', 'DEBUG'])
 
+# Exclude Filter
+parser.add_argument('--excludeFilter', '-x', '-excludeFilters', '-E', '-X', metavar="[]", \
+                    action="append", required=False, nargs='+', help="Exclude filters",\
+                    choices=['U', 'B', 'V', 'R', 'I'])
+
+# Force Flag
+parser.add_argument('--force', default=False, action='store_true',\
+                    help="Force computations without darks or matching gains"
+                    )
+
+# Skip Flat Flag
+parser.add_argument('--no-flat', default=False, action='store_true',\
+                    help="Force reduction pipeline to not use flats"
+                    )
+
 # Specify Version flag
 parser.add_argument('--version', '-V', '-version', action='version', version='%(prog)s Version 0.0, 20231129')
-
 
 ## REQUIRED ARGUMENTS
 required = parser.add_argument_group('required arguments')
 
-# Specify FITS directory
-required.add_argument("--indir",'-I', '-indir', action="store", metavar='dir',type=str, required=True,\
-                    help="Directory containing all raw light and calibration frames",\
+# Specify Src Data FITS directory
+required.add_argument("--datadir",'-D', '-datadir', action="store", metavar='dir',type=str, required=True,\
+                    help="Directory containing all raw light frames",\
+                    )
+
+# Specify Calibration  FITS directory
+required.add_argument("--caldir",'-C', '-caldir', action="store", metavar='dir',type=str, required=True,\
+                    help="Directory containing all calibration frames",\
                     )
 
 # Specify output directory
@@ -61,3 +83,138 @@ redux.logger.debug(vars(redux))
 
 # Read in all FITS file from specified directory
 redux.findFITS()
+
+print("Done sorting files")
+print()
+
+
+#Note self.frameListDict[frame.type][frame.filter][frame.gain][frame.intTime]
+for lightFilter in redux.frameListDict['light'].keys():
+    print("Starting work on new master light frame.")
+    print(f"  Working with light filter: {lightFilter}")
+    for lightGain in redux.frameListDict['light'][lightFilter].keys():
+        print(f"  Working with light gain: {lightGain}")
+
+        for lightIntTime in redux.frameListDict['light'][lightFilter][lightGain]:
+            print(f"  Working with light integration time: {lightIntTime}")
+
+            lights = redux.frameListDict['light'][lightFilter][lightGain][lightIntTime]
+            print(f"    Got lights: {lights}")
+
+            if not redux.no_flat:
+                #TODO: Wrap this in try/except in cases where filter doesn't match -- fail in this case
+                #TODO: Wrap this in tr/except in cases where gain doesn't match -- alert in this case and modify flat Gain
+                #Assume there is only one integration time for this combination
+                flatGain = lightGain
+                flatIntTime = list(redux.frameListDict['flat'][lightFilter][flatGain].keys())[0]
+
+                #TODO: This should not fail if the above didn't exception-out, modify for flat gain issues
+                flats =  redux.frameListDict['flat'][lightFilter][flatGain][flatIntTime]
+                print(f"    Got flats: {flats}")
+
+                #Get Darks with filter=None, gain=lightGain, intTime=flatIntTime
+                #  If none, found raise exception
+                #  If gain is issue, alert and modify dark gain
+                #  If int time is issue, subtract biases out and scale thermal signal
+                # for now, assume all will work ...
+                try:
+                    darksForFlats = redux.frameListDict['dark'][None][lightGain][flatIntTime]
+                except KeyError as e:
+                    print(e,)
+                    print(" Issue with Dark for Flats")
+                    try:
+                        redux.frameListDict['dark']
+                    except KeyError as e:
+                        print("Unable to find darks. Exiting.")
+                        exit()
+                    try:
+                        redux.frameListDict['dark'][None]
+                    except KeyError as e:
+                        print("Dark dictionary improperly formatted. Filter is not None. Exiting.")
+                        exit()
+                    try:
+                        redux.frameListDict['dark'][None][lightGain]
+                    except KeyError as e:
+                        #TODO: Provide override flag for just goofing around!
+                        print("Dark has different gain. Can proceed if --force flag used.")
+                        if  redux.force:
+                            print("Functionality for proceeding with unequal gains not available yet.")
+                            exit()
+                    try:
+                        redux.frameListDict['dark'][None][lightGain][flatIntTime]
+                    except KeyError as e:
+                        #TODO: Provide override flag for just goofing around!
+                        print("Dark has different integration time. Can proceed to scale thermal signal if --force flag used.")
+                        if  redux.force:
+                            print("Functionality providing thermal scaling not available yet")
+                            exit()
+                        else:
+                            print(f"No darks provided that with equal integration time for {flats}.\n"+\
+                                " You can force the computation to proceed by rerunning with --force flag.\n"+\
+                                " The result will likely not be useful for estimating physical parameters.")
+                except Exception as e:
+                    print(e)
+                    exit()
+                print(f"    Got darks for flats: {darksForFlats}")
+
+                masterDarkForFlat = darksForFlats()
+                flats.setDarkFrameObj(masterDarkForFlat)
+
+
+            #Get Darks with filter=None, gain=lightGain, intTime=lightIntTime
+            #  If none, found raise exception
+            #  If gain is issue, alert and modify dark gain
+            #  If int time is issue, subtract biases out and scale thermal signal
+            # for now, assume all will work ...
+            try:
+                darksForLights = redux.frameListDict['dark'][None][lightGain][lightIntTime]
+            except KeyError as e:
+                print(e,)
+                print(" Issue with Dark for Lights")
+                try:
+                    redux.frameListDict['dark']
+                except KeyError as e:
+                    print("Unable to find darks. Exiting.")
+                    exit()
+                try:
+                    redux.frameListDict['dark'][None]
+                except KeyError as e:
+                    print("Dark dictionary improperly formatted. Filter is not None. Exiting.")
+                    exit()
+                try:
+                    redux.frameListDict['dark'][None][lightGain]
+                except KeyError as e:
+                    #TODO: Provide override flag for just goofing around!
+                    print("Dark has different gain. Filter is not None. Can proceed if --force flag used.")
+                    if  redux.force:
+                        pass
+                print("Seems gain or integration times aren't matching with darks/flats.\n"+\
+                      "Further investigation is needed to determine whether darks must be scaled.\n" + e)
+                
+            print(f"    Got darks for lights: {darksForLights}")
+
+ 
+            lights.setDarkFrameObj(darksForLights())
+            if not redux.no_flat:
+                lights.setFlatFrameObj(flats())
+
+
+            finalLight = lights()
+
+            plt.figure(figsize=(12,8))
+            plt.imshow(finalLight.data, \
+                       vmin = finalLight.mean - finalLight.std, vmax = finalLight.mean + finalLight.std, \
+                       cmap='gray'
+                      )
+            
+            plt.title(f"Min,Max: {finalLight.min:0.1f}, {finalLight.max:0.1f}; Median: {finalLight.median:0.1f};"+\
+                      f"Mean: {finalLight.mean:0.1f}; STD: {finalLight.std:0.1f}\n{finalLight}")
+            plt.tight_layout()
+            plt.show()
+            
+
+
+
+            
+
+
