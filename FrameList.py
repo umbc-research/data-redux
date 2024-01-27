c20106ddeb4bb86ca24b36492c56cc262eeb3f10
Roy Prouty <proutyr1@umbc.edu>
2024-01-27 17:56:06 -0500

    Removed calibration math from FrameList Class. It should be more clear in data_redux

--- FrameList.py
+++ FrameList.py
@@ -13,8 +13,8 @@ class FrameList(list):
         self.append(frame)
         self.redux = redux
 
-        self._darkFrame = 0
-        self._flatFrame = 1
+        self._darkFrame = None
+        self._flatFrame = None
         self._master = None
 
     def setDarkFrame(self, darkFrame):
@@ -22,36 +22,6 @@ class FrameList(list):
 
     def setFlatFrame(self, flatFrame):
         self._flatFrame = flatFrame
-
-
-    def __call__(self): #Calculate master frame
-        #This is where all of the master frame accumulation is done 
-        darkCorr = False
-        flatCorr = False
-        #TODO: Remove logging from this function, wrap calling functions with logging
-        if self._darkFrame == 0:
-            self.redux.logger.debug(f"_darkFrame is 0, {self._darkFrame}")
-            self.redux.logger.debug("Deriving Master Dark")
-            data = np.median([s() for s in self], axis=0)
-        else: # does not include functionality for mismatched dark/thermal frame integration times
-            self.redux.logger.debug(f"_darkFrame is non-0, {self._darkFrame}")
-            self.redux.logger.debug("Deriving Master Dark")
-            data = np.median([s()-self._darkFrame() for s in self], axis=0)
-            darkCorr = True
-
-        self.redux.logger.debug(f"Dark Done. Checking on Flats")
-        if self._flatFrame != 1:
-            self.redux.logger.debug(f"_flatFrame is not 1, {self._flatFrame}")
-            data /= (self._flatFrame() / self._flatFrame.max)
-            flatCorr = True
-        else:
-            self.redux.logger.debug(f"_flatFrame is 1 -- doing nothing., {self._darkFrame}")
-
-        self.redux.logger.debug(f"Creating Master Frame Obj is 0, {self._darkFrame}")
-        self._master = Frame(data, 'master', self[0].filter, self[0].gain, self[0].intTime, self[0].header)
-        self._master.flatCorr = flatCorr
-        self._master.darkCorr = darkCorr
-        self.redux.logger.debug(f"Completing FrameList() call, {self}")
 
     def getMaster(self):
         self.redux.logger.debug(f"Retrieving Master, {self._master}")
